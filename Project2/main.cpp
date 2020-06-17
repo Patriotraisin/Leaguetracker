b25a5e8eecce19928241484394cf51fb47541019
JACK #EZ <jack@mayers-air.lan>
2020-06-18 00:19:29 +0800

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by JACK #EZ on 18/6/2020.
+//  Copyright © 2020 JACK. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
